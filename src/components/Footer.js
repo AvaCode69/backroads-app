862cc5b48c15a5dbdb4d31b60510842a0cd6c840
AvaCode69 <reacthmd34@gmail.com>
2023-04-10 17:54:25 +0200

    broke some components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,11 @@
 import React from "react";
-import { pageLinks, socilaLink } from "../data";
+import PageLinks from "./PageLinks";
+import { socilaLink } from "../data";
 
 const Footer = () => {
   return (
     <footer className="section footer">
-      <ul className="footer-links">
-        {pageLinks.map((link) => {
-          const { id, href, text } = link;
-          return (
-            <li key={id}>
-              <a href={href} className="footer-link">
-                {text}
-              </a>
-            </li>
-          );
-        })}
-      </ul>
+      <PageLinks parentClass="footer-links" itemClass="footer-link" />
       <ul className="footer-icons">
         {socilaLink.map((link) => {
           const { id, href, icon } = link;
